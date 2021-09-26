d02e0b0d4fb427b65b74858ccd89560517ff0307
kleo-53 <liza.1610@mail.ru>
2021-09-26 16:26:49 +0300

    07.09 swap

---
+++ Homework2.c
@@ -0,0 +1,38 @@
+﻿#include <stdio.h>
+#include <string.h>
+
+const char* checkBalance(char str[])
+{
+    int counter = 0;
+
+    for (int i = 0; i < strlen(str); ++i)
+    {
+        if (str[i] == '(')
+        {
+            ++counter;
+        }
+        else if (str[i] == ')')
+        {
+            if (counter <= 0)
+            {
+                return "String is not balanced.";
+            }
+            --counter;
+        }
+    }
+    if (counter != 0)
+    {
+        return "String is not balanced.";
+    }
+    return "String is balanced.";
+}
+
+int main()
+{
+    int counter = 0;
+    char str[40];
+
+    printf("Enter a string consisting of brackets: ");
+    scanf("%s", &str);
+    printf("%s\n", checkBalance(str));
+}
