6aac0f1228c50186550f08f4b84400acda66b18e
kleo-53 <liza.1610@mail.ru>
2021-09-26 16:28:49 +0300

    07.09 bracket balance

---
+++ Homework5.c
@@ -0,0 +1,32 @@
+﻿#include <stdio.h>
+#include <stdbool.h>
+
+bool isPrime(int number)
+{
+    for (int i = 2; i * i <= number; ++i)
+    {
+        if (number % i == 0)
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+int main()
+{
+    int number = 0;
+
+    printf("Enter a number: ");
+    scanf("%d", &number);
+    printf("This numbers up to the entered are prime:\n");
+
+    for (int i = 2; i <= number; ++i)
+    {
+        if (isPrime(i)) 
+        {
+            printf("%d\n", i);
+        }
+    }
+
+}
