ff1a9be618ca0fe0fe4a380010edc853c8b07f4c
kleo-53 <liza.1610@mail.ru>
2021-09-26 16:27:44 +0300

    07.09 incomplete quotient

---
+++ Homework3.c
@@ -0,0 +1,16 @@
+﻿#include <stdio.h>
+
+int main()
+{
+    int first = 0;
+    int second = 0;
+
+    printf("Enter two integers separated by a space:");
+    scanf("%d %d", &first, &second);
+
+    first = first ^ second;
+    second = second ^ first;
+    first = first ^ second;
+
+    printf("The program swapped numbers without using additional variables: %d %d\n", first, second);
+}
