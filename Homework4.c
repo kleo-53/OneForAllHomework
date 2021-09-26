e2d7217a691624cb21f396680bb668356fa83a1e
kleo-53 <liza.1610@mail.ru>
2021-09-26 16:28:15 +0300

    07.09 lucky tickets

---
+++ Homework4.c
@@ -0,0 +1,25 @@
+﻿#include <stdio.h>
+#define arraySize 40
+
+int main()
+{
+    int size = 0;
+    int massiv[arraySize];
+    int element = 0;
+    int counter = 0;
+
+    printf("Enter a size of array: ");
+    scanf("%d", &size);
+    printf("Enter array elements: \n");
+
+    for (int i = 0; i < size; ++i)
+    {
+        scanf("%d", &massiv[i]);
+        if (massiv[i] == 0)
+        {
+            ++counter;
+        }
+    }
+
+    printf("This array has %d zero elements.\n", counter);
+}
