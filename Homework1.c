3358ee1887498402091e39e6821be053b2c8e451
kleo-53 <liza.1610@mail.ru>
2021-09-26 16:25:44 +0300

    07.09 zero elements

---
+++ Homework1.c
@@ -0,0 +1,29 @@
+﻿#include <stdio.h>
+
+#define arraySize 28
+
+int main()
+{
+
+    int counter[arraySize] = { 0 };
+
+    for (int i = 0; i < 10; ++i)
+    {
+        for (int j = 0; j < 10; ++j)
+        {
+            for (int k = 0; k < 10; ++k)
+            {
+                ++counter[i + j + k];
+            }
+        }
+    }
+
+    int answer = 0;
+
+    for (int i = 0; i < arraySize; ++i)
+    {
+        answer += counter[i] * counter[i];
+    }
+
+    printf("Number of lucky tickets: %d", answer);
+}
