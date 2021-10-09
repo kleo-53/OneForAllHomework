23ffca2901aacc545f1aa5a47f88280368b47235
kleo-53 <liza.1610@mail.ru>
2021-10-09 23:24:43 +0300

    now information from the file is stored in a local array

--- hw4/phonebook/phonebook.c
+++ hw4/phonebook/phonebook.c
@@ -1,158 +1,101 @@
-﻿#include <stdio.h>
+﻿#pragma warning (disable : 6031)
+
+#include <stdio.h>
 #include <time.h>
 #include <stdbool.h>
 #include <stdlib.h>
+#include <string.h>
 
 #define ARRAY_SIZE 100
 #define RANGE 11
-typedef struct PhoneBookEntry {
+#define _CRT_DEFINE_NO_WARNINGS
+
+typedef struct PhoneBookEntry 
+{
     char name[30];
     char phone[30];
 } PhoneBookEntry;
 
-struct PhoneBookEntry GlobalEntry[ARRAY_SIZE];
-int currentRecord = 0;
-
-void clrscr()
-{
-    system("@cls||clear");
-}
-
-int addRecord()
+void addRecord(PhoneBookEntry *phoneBook, int phoneBookSize)
 {
     PhoneBookEntry entry;
-
     printf("\nEnter name and surname: ");
-    scanf("%[^\n]%*c", entry.name);
+    scanf("%[^\n]%*c", &entry.name);
 
     printf("Enter phone number: ");
-    scanf("%[^\n]%*c", entry.phone);
-    GlobalEntry[currentRecord] = entry;
-    ++currentRecord;
-    printf("Record has been added successfully!");
-    return 0;
+    scanf("%[^\n]%*c", &entry.phone);
+    phoneBook[phoneBookSize] = entry;
+    printf("Record has been added successfully!\n");
 }
 
-int printRecords()
+void printRecords(PhoneBookEntry* phoneBook, int phoneBookSize)
 {
-    FILE* file = fopen("input.txt", "r");
-    if (file == NULL) {
-        printf("The file is empty!");
-        return 1;
-    }
-    PhoneBookEntry entry;
-    int linesRead = 0;
-    if (feof(file))
+    printf("\nThere are all records in file:\n");
+    for (int i = 0; i < phoneBookSize; i++)
     {
-        printf("\nThe file is empty!");
-        fclose(file);
-        return 0;
+        printf("%s: %s\n", phoneBook[i].name, phoneBook[i].phone);
     }
-    printf("\nThere are all records in file:");
-    while (!feof(file)) {
-        char* buffer = malloc(sizeof(char) * 30);
-        const int readBytes = fscanf(file, "%[^\n]%*c", buffer);
-        if (readBytes < 0) {
-            break;
-        }
-        printf("\n%s", buffer);
-        ++linesRead;
-    }
-    fclose(file);
-    return 0;
 }
 
-int findNumber()
+void findNumber(PhoneBookEntry* phoneBook, int phoneBookSize)
 {
     printf("\nEnter name: ");
     char inputName[30] = "";
-    scanf("%[^\n]%*c", inputName);
-    PhoneBookEntry entry;
+    scanf("%[^\n]%*c", &inputName);
 
-    FILE* file = fopen("input.txt", "r");
-    if (file == NULL) {
-        printf("The file is empty!");
-        return 1;
-    }
-
-    int linesRead = 0;
     bool findUser = false;
-    while (!feof(file)) {
-        fscanf(file, "%[^:]%*c%*c", entry.name);
-        fscanf(file, "%[^\n]%*c", entry.phone);
-        if (entry.name < 0 || entry.phone < 0) {
-            break;
-        }
-        if (strcmp(entry.name, inputName) == 0)
+    for (int i = 0; i < phoneBookSize; ++i)
+    {
+        if (strcmp(phoneBook[i].name, inputName) == 0)
         {
-            printf("User with name %s has number %s", entry.name, entry.phone);
+            printf("User with name %s has number %s\n", phoneBook[i].name, phoneBook[i].phone);
             findUser = true;
         }
-        ++linesRead;
     }
-    fclose(file);
     if (!findUser)
     {
-        printf("There aren't any records with such name:(");
+        printf("There aren't any records with such name:(\n");
     }
-    return 0;
 }
 
-int findName()
+void findName(PhoneBookEntry* phoneBook, int phoneBookSize)
 {
     printf("\nEnter phone: ");
     char inputNumber[30] = "";
-    scanf("%[^\n]%*c", inputNumber);
-    PhoneBookEntry entry;
+    scanf("%[^\n]%*c", &inputNumber);
 
-    FILE* file = fopen("input.txt", "r");
-    if (file == NULL) {
-        printf("The file is empty!");
-        return 1;
-    }
-
-    int linesRead = 0;
     bool findUser = false;
-    while (!feof(file)) {
-        fscanf(file, "%[^:]%*c%*c", entry.name);
-        fscanf(file, "%[^\n%]%*c", entry.phone);
-        if (entry.name < 0 || entry.phone < 0) {
-            break;
-        }
-        if (strcmp(entry.phone, inputNumber) == 0)
+    for (int i = 0; i < phoneBookSize; ++i)
+    {
+        if (strcmp(phoneBook[i].phone, inputNumber) == 0)
         {
-            printf("User with number %s has name %s", entry.phone, entry.name);
+            printf("User with name %s has number %s\n", phoneBook[i].name, phoneBook[i].phone);
             findUser = true;
         }
-        ++linesRead;
     }
-    fclose(file);
     if (!findUser)
     {
-        printf("There aren't any records with such number:(");
+        printf("There aren't any records with such number:(\n");
     }
-    return 0;
 }
 
-int saveAll()
+void saveAll(PhoneBookEntry* phoneBook, int phoneBookSize, int addNumber)
 {
     FILE* out = fopen("input.txt", "a");
-    if (out == NULL) {
+    if (out == NULL) 
+    {
         return -1;
     }
-    for (int i = 0; i < currentRecord; ++i) 
+    for (int i = phoneBookSize; i < phoneBookSize + addNumber; ++i)
     {
-        fprintf(out, "\n%s: %s", GlobalEntry[i].name, GlobalEntry[i].phone);
+        fprintf(out, "\n%s: %s", phoneBook[i].name, phoneBook[i].phone);
     }
-    printf("\nRecords has been added to file successfully!");
+    printf("\nRecords has been added to file successfully!\n");
     fclose(out);
-    currentRecord = 0;
-    return 0;
 }
 
-bool menu()
+int menu(PhoneBookEntry *phoneBook, int phoneBookSize, int addNumber)
 {
-    char command;
     printf("\n0 - Finish work");
     printf("\n1 - Add record (name and number)");
     printf("\n2 - Print all recordinds");
@@ -161,38 +104,64 @@ bool menu()
     printf("\n5 - Save data in file");
     printf("\nChose the command: ");
 
-    command = getche();
+    int command = getche();
     printf("\n");
     switch (command) {
     case '0':
-        return false;
+        return -1;
     case '1':
-        addRecord();
+        addRecord(phoneBook, phoneBookSize + addNumber);
+        ++addNumber;
         break;
     case '2':
-        printRecords();
+        printRecords(phoneBook, phoneBookSize + addNumber);
         break;
     case '3':
-        findNumber();
+        findNumber(phoneBook, phoneBookSize + addNumber);
         break;
     case '4':
-        findName();
+        findName(phoneBook, phoneBookSize + addNumber);
         break;
     case '5':
-        saveAll();
+        saveAll(phoneBook, phoneBookSize, addNumber);
+        addNumber = -1000;
         break;
     default:
-        printf("\nNo option selected");
+        printf("\nNo option selected\n");
     }
-    return true;
+    return addNumber;
 }
 
 int main()
 {
-    unsigned int sleepTime = 2000;
-    while (menu())
+    FILE* file = fopen("input.txt", "r");
+    if (file == NULL) 
     {
-        _sleep(sleepTime);
-        clrscr();
+        printf("The file is empty!\n");
+        return 1;
+    }
+
+    int linesRead = 0;
+    PhoneBookEntry phoneBook[100];
+    while (!feof(file)) 
+    {
+        fscanf(file, "%[^:]%*c%*c", &phoneBook[linesRead].name);
+        fscanf(file, "%[^\n%]%*c", &phoneBook[linesRead].phone);
+        ++linesRead;
+    }
+    int addNumber = 0;
+    int result = menu(phoneBook, linesRead, addNumber);
+    while (result != -1)
+    {
+        if (result == -1000)
+        {
+            linesRead += addNumber;
+            addNumber = 0;
+        }
+        else
+        {
+            addNumber = result;
+        }
+        result = menu(phoneBook, linesRead, addNumber);
     }
 }
