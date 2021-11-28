cabc95e6a0b532768b75e05d278f1157e5a96f1e
Elizaveta Karaseva <46417276+kleo-53@users.noreply.github.com>
2021-11-28 12:29:46 +0300

    task1 (#21)
    * task1
    
    * Update task1.c
    
    * task2
    
    * Update task2.c
    
    * Update task2.c

---
+++ kr2/task1/task1/task1.c
@@ -0,0 +1,93 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include "list.h"
+
+bool testWorkCase()
+{
+    List* myList = createList();
+    bool isAdded = addData(myList, 10);
+    isAdded = addData(myList, 11) && isAdded;
+    isAdded = addData(myList, 1) && isAdded;
+    isAdded = addData(myList, 843) && isAdded;
+    isAdded = addData(myList, -5) && isAdded;
+    if (!isAdded)
+    {
+        deleteList(myList);
+        return false;
+    }
+    reverse(&myList);
+    FILE* inFile = fopen("test.txt", "w");
+    printListInFile(myList, inFile);
+    fclose(inFile);
+    char* fileString = calloc(40, sizeof(char));
+    if (fileString == NULL)
+    {
+        deleteList(myList);
+        return false;
+    }
+    FILE* outFile = fopen("test.txt", "r");
+    fscanf_s(outFile, "%[^\n]s", fileString, 40);
+    fclose(outFile);
+    char* correctString = { "-5 843 1 11 10 " };
+    if (strcmp(fileString, correctString) != 0)
+    {
+        deleteList(myList);
+        return false;
+    }
+    isAdded = addData(myList, -5);
+    isAdded = addData(myList, 15) && isAdded;
+    if (!isAdded)
+    {
+        deleteList(myList);
+        return false;
+    }
+    reverse(&myList);
+    FILE* inFile2 = fopen("test.txt", "w");
+    printListInFile(myList, inFile2);
+    fclose(inFile2);
+    FILE* outFile2 = fopen("test.txt", "r");
+    strcat(fileString, "");
+    fscanf_s(outFile2, "%[^\n]s", fileString, 40);
+    fclose(outFile2);
+    correctString = "15 -5 10 11 1 843 -5 ";
+    deleteList(myList);
+    return strcmp(fileString, correctString) == 0;
+}
+
+int main()
+{
+    if (!testWorkCase())
+    {
+        printf("Tests failed:(");
+        return -1;
+    }
+    List* myList = createList();
+    FILE* file = fopen("input.txt", "r");
+    if (file == NULL)
+    {
+        return -1;
+    }
+    int inputNumber = 0;
+    bool isWork = true;
+    while (!feof(file)) 
+    {
+        fscanf_s(file, "%d", &inputNumber);
+        isWork = addData(myList, inputNumber);
+        if (!isWork)
+        {
+            printf("Some errors were occured");
+            return -1;
+        }
+    }
+    fclose(file);
+    printf("This is the given list:\n");
+    printList(myList);
+    reverse(&myList);
+    printf("\nThis is the list after reversing:\n");
+    printList(myList);
+    deleteList(myList);
+    return 0;
+}
---
+++ kr2/task2.c
@@ -0,0 +1,60 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdlib.h>
+#include <stdio.h>
+#define ARRAY_SIZE 50
+
+int main()
+{
+    FILE* file = fopen("input.txt", "r");
+    if (file == NULL)
+    {
+        return -1;
+    }
+    int n = 0;
+    fscanf_s(file, "%d", &n);
+    int matrix[ARRAY_SIZE][ARRAY_SIZE] = { 0 };
+    int inputNumber = 0;
+    int i = 0;
+    int j = 0;
+    while (!feof(file))
+    {
+        fscanf_s(file, "%d", &inputNumber);
+        matrix[i][j] = inputNumber;
+        if (j % n == n - 1) 
+        {
+            j = 0;
+            ++i;
+        }
+        else
+        {
+            ++j;
+        }
+    }
+    fclose(file);
+    for (int k = 1; k < n; ++k)
+    {
+        for (int i = 1; i < n; ++i)
+        {
+            for (int j = 1; j < n; ++j)
+            {
+                matrix[i][j] = matrix[i][j] || (matrix[i][k] && matrix[k][j]);
+            }
+        }
+    }
+    FILE* outfile = fopen("output.txt", "w");
+    if (file == NULL)
+    {
+        return -1;
+    }
+    for (int i = 0; i < n; ++i)
+    {
+        for (int j = 0; j < n; ++j)
+        {
+            fprintf(outfile, "%d ", matrix[i][j]);
+        }
+        fprintf(outfile, "\n");
+    }
+    fclose(outfile);
+    return 0;
+}
