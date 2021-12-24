46c1c08623e54453a57849913b2eb124d154fae3
kleo-53 <liza.1610@mail.ru>
2021-12-24 11:24:36 +0300

    fixed style guide

--- hw11-RabinCarp/RabinCarp.c
+++ hw11-RabinCarp/RabinCarp.c
@@ -8,71 +8,73 @@
 
 long long hash(const char* line, const int start, const int end, const long long module, const int base)
 {
-	int hash = 0;
-	for (int i = start; i < end; i++) {
-		hash = (hash * base + line[i] - 31) % module;
-	}
-	if (hash < 0)
-	{
-		hash += module;
-	}
-	return hash;
+    long long hash = 0;
+    for (int i = start; i < end; i++) 
+    {
+        hash = (hash * base + line[i] - 31) % module;
+    }
+    if (hash < 0)
+    {
+        hash += module;
+    }
+    return hash;
 }
 
 long long binPower(long long number, int power, const long long module)
 {
-	int res = 1;
-	while (power)
-		if (power & 1) {
-			res = (res * number) % module;
-			--power;
-		}
-		else {
-			number *= number;
-			power >>= 1;
-		}
-	return res;
+    long long res = 1;
+    while (power)
+    {
+        if (power & 1) {
+            res = (res * number) % module;
+            --power;
+        }
+        else {
+            number *= number;
+            power >>= 1;
+        }
+    }
+    return res;
 }
 
-int rollingHash(const long long previousHash, char* line, const int newSymbol, const long long module, 
-	const int previousPower, const int base, const int patternSize)
+long long rollingHash(const long long previousHash, const char* line, const int newSymbol, const long long module, 
+    const long long previousPower, const int base, const int patternSize)
 {
-	return ((previousHash - (line[newSymbol - 1] - 31) * previousPower) * base + line[newSymbol + patternSize - 1] - 31) % module;
+    return ((previousHash - (line[newSymbol - 1] - 31) * previousPower) * base + line[newSymbol + patternSize - 1] - 31) % module;
 }
 
-bool isSimple(const char* textLine, const int start, const int end, char* pattern)
+bool isEqual(const char* textLine, const int start, const int end, const char* pattern)
 {
-	for (int i = start; i < end; ++i)
-	{
-		if (textLine[i] != pattern[i - start])
-		{
-			return false;
-		}
-	}
-	return true;
+    for (int i = start; i < end; ++i)
+    {
+        if (textLine[i] != pattern[i - start])
+        {
+            return false;
+        }
+    }
+    return true;
 }
 
-int RabinKarp(const char* textLine, const char* pattern)
+int rabinKarp(const char* textLine, const char* pattern)
 {
-	const int textSize = strlen(textLine);
-	const int patternSize = strlen(pattern);
-    const long long hsub = hash(pattern, 0, patternSize, MODULE, BASE_NUMBER);
-	long long hs = hash(textLine, 0, patternSize, MODULE, BASE_NUMBER);
-	const long long previousPower = binPower(BASE_NUMBER, patternSize - 1, MODULE);
-	int flag = 0;
-	for (int i = 1; i < (textSize - patternSize + 2); ++i)
-	{
-		if (hs == hsub) 
-		{
-			if (isSimple(textLine, i - 1, i + patternSize - 1, pattern))
-			{
-				return i;
-			}
-		}
-		hs = rollingHash(hs, textLine, i, MODULE, previousPower, BASE_NUMBER, patternSize);
-		if (hs < 0) {
-			hs += MODULE;
-		}
-	}
-	return -1;
+    const int textSize = (int)strlen(textLine);
+    const int patternSize = (int)strlen(pattern);
+    const long long patternHash = hash(pattern, 0, patternSize, MODULE, BASE_NUMBER);
+    long long currentHash = hash(textLine, 0, patternSize, MODULE, BASE_NUMBER);
+    const long long previousPower = binPower(BASE_NUMBER, patternSize - 1, MODULE);
+    for (int i = 1; i < (textSize - patternSize + 2); ++i)
+    {
+        if (currentHash == patternHash)
+        {
+            if (isEqual(textLine, i - 1, i + patternSize - 1, pattern))
+            {
+                return i;
+            }
+        }
+        currentHash = rollingHash(currentHash, textLine, i, MODULE, previousPower, BASE_NUMBER, patternSize);
+        if (currentHash < 0) {
+            currentHash += MODULE;
+        }
+    }
+    return -1;
 }
--- hw11-RabinCarp/main.c
+++ hw11-RabinCarp/main.c
@@ -9,107 +9,107 @@
 
 bool testSimpleCase()
 {
-	char inputLine[30] = { "cadab" };
-	FILE* file = fopen("test.txt", "r");
-	if (file == NULL)
-	{
-		free(inputLine);
-		return false;
-	}
-	char fileLine[STR_SIZE] = { " " };
-	int numberLine = 1;
-	while (!feof(file))
-	{
-		fgets(&fileLine, STR_SIZE, file);
-		int resultRK = RabinKarp(fileLine, inputLine);
-		if (numberLine == 1 && resultRK == 6)
-		{
-			return true;
-		}
-		++numberLine;
-	}
-	return false;
+    char inputLine[30] = { "cadab" };
+    FILE* file = fopen("test.txt", "r");
+    if (file == NULL)
+    {
+        free(inputLine);
+        return false;
+    }
+    char fileLine[STR_SIZE] = { " " };
+    int numberLine = 1;
+    while (!feof(file))
+    {
+        fgets(&fileLine, STR_SIZE, file);
+        int resultRK = rabinKarp(fileLine, inputLine);
+        if (numberLine == 1 && resultRK == 6)
+        {
+            return true;
+        }
+        ++numberLine;
+    }
+    return false;
 }
 
 bool testSmallCase()
 {
-	char inputLine[30] = { "ncarp" };
-	FILE* file = fopen("test.txt", "r");
-	if (file == NULL)
-	{
-		free(inputLine);
-		return false;
-	}
-	char fileLine[STR_SIZE] = { " " };
-	int numberLine = 1;
-	while (!feof(file))
-	{
-		fgets(&fileLine, STR_SIZE, file);
-		int resultRK = RabinKarp(fileLine, inputLine);
-		if (numberLine == 2 && resultRK == 5)
-		{
-			return true;
-		}
-		++numberLine;
-	}
-	return false;
+    char inputLine[30] = { "ncarp" };
+    FILE* file = fopen("test.txt", "r");
+    if (file == NULL)
+    {
+        free(inputLine);
+        return false;
+    }
+    char fileLine[STR_SIZE] = { " " };
+    int numberLine = 1;
+    while (!feof(file))
+    {
+        fgets(&fileLine, STR_SIZE, file);
+        int resultRK = rabinKarp(fileLine, inputLine);
+        if (numberLine == 2 && resultRK == 5)
+        {
+            return true;
+        }
+        ++numberLine;
+    }
+    return false;
 }
 
 bool testHardCase()
 {
-	char inputLine[30] = { "ada bra aa" };
-	FILE* file = fopen("test.txt", "r");
-	if (file == NULL)
-	{
-		free(inputLine);
-		return false;
-	}
-	char fileLine[STR_SIZE] = { " " };
-	int numberLine = 1;
-	while (!feof(file))
-	{
-		fgets(&fileLine, STR_SIZE, file);
-		int resultRK = RabinKarp(fileLine, inputLine);
-		if (numberLine == 1 && resultRK == 20)
-		{
-			return true;
-		}
-		++numberLine;
-	}
-	return false;
+    char inputLine[30] = { "ada bra aa" };
+    FILE* file = fopen("test.txt", "r");
+    if (file == NULL)
+    {
+        free(inputLine);
+        return false;
+    }
+    char fileLine[STR_SIZE] = { " " };
+    int numberLine = 1;
+    while (!feof(file))
+    {
+        fgets(&fileLine, STR_SIZE, file);
+        int resultRK = rabinKarp(fileLine, inputLine);
+        if (numberLine == 1 && resultRK == 20)
+        {
+            return true;
+        }
+        ++numberLine;
+    }
+    return false;
 }
 
 
 int main()
 {
-	if (!testSmallCase() || !testSimpleCase() || !testHardCase())
-	{
-		printf("Tests failed.");
-		return 0;
-	}
-	printf("Enter text line:\n");
-	char inputLine[STR_SIZE] = { " " };
-	scanf_s("%[^\n]", &inputLine, STR_SIZE);
+    if (!testSmallCase() || !testSimpleCase() || !testHardCase())
+    {
+        printf("Tests failed.");
+        return 0;
+    }
+    printf("Enter text line:\n");
+    char inputLine[STR_SIZE] = { " " };
+    scanf_s("%[^\n]", &inputLine, STR_SIZE);
 
-	FILE* file = fopen("input.txt", "r");
-	if (file == NULL)
-	{
-		free(inputLine);
-		return -1;
-	}
-	char fileLine[STR_SIZE] = { " " };
-	int numberLine = 1;
-	while (!feof(file))
-	{
-		fgets(&fileLine, STR_SIZE, file);
-		int resultRK = RabinKarp(fileLine, inputLine);
-		if (resultRK != -1)
-		{
-			printf("This line first encountered in a file at line %d, position %d.", numberLine, resultRK);
-			return 0;
-		}
-		++numberLine;
-	}
-	printf("There is not such line in file:(");
-	return 0;
+    FILE* file = fopen("input.txt", "r");
+    if (file == NULL)
+    {
+        free(inputLine);
+        return -1;
+    }
+    char fileLine[STR_SIZE] = { " " };
+    int numberLine = 1;
+    while (!feof(file))
+    {
+        fgets(&fileLine, STR_SIZE, file);
+        int resultRK = rabinKarp(fileLine, inputLine);
+        if (resultRK != -1)
+        {
+            printf("This line first encountered in a file at line %d, position %d.", numberLine, resultRK);
+            return 0;
+        }
+        ++numberLine;
+    }
+    printf("There is not such line in file:(");
+    return 0;
 }
