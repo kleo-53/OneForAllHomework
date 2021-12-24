6136658f350e49b9e0228aadf5178534b046b72e
Elizaveta Karaseva <46417276+kleo-53@users.noreply.github.com>
2021-12-24 12:14:34 +0300

    kr2.1 (#23)
    * task1
    
    * i did not make entery :(((((

---
+++ kr3/task1/task1.c
@@ -0,0 +1,92 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include "List.h"
+
+bool testWorkCase()
+{
+    List* myList = createList(3, 5);
+    bool isWorking = addData(myList, 9);
+    isWorking = addData(myList, 9) && isWorking;
+    isWorking = addData(myList, 8) && isWorking;
+    isWorking = addData(myList, 6) && isWorking;
+    isWorking = addData(myList, 5) && isWorking;
+    isWorking = addData(myList, 4) && isWorking;
+    isWorking = addData(myList, 3) && isWorking;
+    isWorking = addData(myList, 3) && isWorking;
+    isWorking = addData(myList, 1) && isWorking;
+    isWorking = addData(myList, -3) && isWorking;
+    if (!isWorking)
+    {
+        deleteList(myList);
+        return false;
+    }
+    FILE* output = fopen("test.txt", "w");
+    if (output == NULL)
+    {
+        deleteList(myList);
+        return -1;
+    }
+    printListInFile(myList, output);
+    fclose(output);
+    deleteList(myList);
+    FILE* testFile = fopen("test.txt", "r");
+    char* testString = malloc(sizeof(char));
+    if (testFile == NULL || testString == NULL)
+    {
+        fclose(testFile);
+        free(testString);
+        return false;
+    }
+    fgets(testString, 42, testFile);
+    fclose(testFile);
+    return strcmp(testString, "1 -3 5 4 3 3 9 9 8 6 ") == 0;
+}
+
+int main()
+{
+    if (!testWorkCase())
+    {
+        printf("Tests failed:(");
+        return -1;
+    }
+    int inputNumberA = 0;
+    int inputNumberB = 0;
+    int inputNumber = 0;
+    bool isWorking = true;
+    printf("Enter a and b:\n");
+    scanf_s("%d", &inputNumberA);
+    scanf_s("%d", &inputNumberB);
+    List* myList = createList(inputNumberA, inputNumberB);
+    FILE* file = fopen("f.txt", "r");
+    if (file == NULL)
+    {
+        deleteList(myList);
+        return -1;
+    }
+    while (!feof(file))
+    {
+        fscanf_s(file, "%d", &inputNumber);
+        isWorking = addData(myList, inputNumber);
+        if (!isWorking)
+        {
+            printf("Some errors were occured");
+            deleteList(myList);
+            fclose(file);
+            return -1;
+        }
+    }
+    fclose(file);
+    FILE* output = fopen("g.txt", "w");
+    if (file == NULL)
+    {
+        deleteList(myList);
+        return -1;
+    }
+    printListInFile(myList, output);
+    fclose(output);
+    deleteList(myList);
+    return 0;
+}
---
+++ kr3/task2/taskGraph/List.h
@@ -0,0 +1,24 @@
+#pragma once
+#include <stdio.h>
+#include <stdbool.h>
+
+// This is strust of list
+typedef struct List List;
+
+// This strust consists of elements position
+typedef struct Position Position;
+
+// This function creates new list
+List* createList();
+
+// This function deletes list
+void deleteList(List* list);
+
+// This function finds suitable position to value and add them to list in given orger
+bool addData(List* list, int value);
+
+// This function prints the list
+void printList(List* list);
+
+// This function prints the list into test file
+void printListInFile(List* list, FILE* file);
